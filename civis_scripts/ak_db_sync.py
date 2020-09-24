6e916eb21255db449ecbb134fb153c215e1e0925
Yotam <paxarchy@gmail.com>
2020-09-24 14:58:51 -0400

    hopefully this is the right way to force this to be a datetime

--- civis_scripts/ak_db_sync.py
+++ civis_scripts/ak_db_sync.py
@@ -167,7 +167,12 @@ def table_sync_incremental_upsert(self, source_table, destination_table, primary
 
     # Get the max source table and destination table primary key
     dest_max_updated = destination_tbl.max_primary_key(updated_col)
-    source_max_updated = source_tbl.max_primary_key(updated_col)
+    dest_max_updated = self.dest_db.query(
+        f"SELECT CAST(MAX({updated_col}) AS DATETIME) FROM {destination_table}"
+    ).first
+    source_max_updated = self.source_db.query(
+        f"SELECT CAST(MAX({updated_col}) AS DATETIME) FROM {source_table}"
+    ).first
 
     # Check for a mismatch in row counts; if dest_max_pk is None, or destination is empty
     # and we don't have to worry about this check.
