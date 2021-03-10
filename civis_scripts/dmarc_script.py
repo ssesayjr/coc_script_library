049bc7772e25f79eb55707d15ca7e2be4c907549
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2021-03-10 11:27:03 -0500

    Update dmarc_script.py

--- civis_scripts/dmarc_script.py
+++ civis_scripts/dmarc_script.py
@@ -90,7 +90,7 @@ def main():
     user_table = 'coc_dmarc.dmarc_table'
 
     if not rs.table_exists(user_table): #if the table doesn't exist create it
-       rs.query(f"create table {user_table} (org_name varchar(2048), email varchar(2048), report_ID varchar(2048), date_range_begin timestamp,date_range_end timestamp, domain varchar(1024), adkim varchar(1024), aspf varchar(1024), p varchar(1024), sp timestamp, pct varchar(1024), source_ip varchar(1024), disposition varchar(1024), dkim varchar(1024), spf varchar(1024));")
+       rs.query(f"create table {user_table} (orgname varchar(2048), email varchar(2048), reportid varchar(2048), date_range_begin timestamp,date_range_end timestamp, domain varchar(1024), adkim varchar(1024), aspf varchar(1024), p varchar(1024), sp timestamp, pct varchar(1024), source_ip varchar(1024), disposition varchar(1024), dkim varchar(1024), spf varchar(1024));")
 
             
     def file_To_Table(file):
@@ -183,9 +183,9 @@ def main():
                 end = column_data.find('</result>')
                 result_substring = column_data[start:end]    
     
-        df = pd.DataFrame({'Org Name':[org_substring],
-                        'Email': [email_substring],
-                        'Report ID': [id_substring],
+        df = pd.DataFrame({'orgname':[org_substring],
+                        'email': [email_substring],
+                        'reportid': [id_substring],
                         'Date Range Begin': [begin_substring],
                         'Date Range End': [end_substring],
                         'Domain': [domain_substring],
