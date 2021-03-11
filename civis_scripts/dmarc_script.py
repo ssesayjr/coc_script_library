1098b929866b9657ca8b0e34e7fc3a4a352c6756
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2021-03-10 22:42:11 -0500

    Update dmarc_script.py

--- civis_scripts/dmarc_script.py
+++ civis_scripts/dmarc_script.py
@@ -102,86 +102,122 @@ def main():
                 start = column_data.find('<org_name>') + len('<org_name>')
                 end = column_data.find('</org_name>')
                 org_substring = column_data[start:end]
+                
+            else: org_substring = 'NA'
+                    
 
             if '<email>' in column_data:
                 start = column_data.find('<email>') + len('<email>')
                 end = column_data.find('</email>')
                 email_substring = column_data[start:end]
+                
+            else: email_substring = 'NA'
 
             if '<report_id>' in column_data:
                 start = column_data.find('<report_id>') + len('<report_id>')
                 end = column_data.find('</report_id>')
                 id_substring = column_data[start:end]
+                
+            else: id_substring = 'NA'
       
             if '<begin>' in column_data:
                 start = column_data.find('<begin>') + len('<begin>')
                 end = column_data.find('</begin>')
                 begin_substring = column_data[start:end]
                 
+            else: begin_substring = 'NA'
+                
             if '<end>' in column_data:
                 start = column_data.find('<end>') + len('<end>')
                 end = column_data.find('</end>')
                 end_substring = column_data[start:end]
+            
+            else: end_substring = 'NA'
                 
                 
             if '<domain>' in column_data:
                 start = column_data.find('<domain>') + len('<domain>')
                 end = column_data.find('</domain>')
-                domain_substring = column_data[start:end]                
+                domain_substring = column_data[start:end] 
+                
+            else: domain_substring = 'NA'
     
     
             if '<adkim>' in column_data:
                 start = column_data.find('<adkim>') + len('<adkim>')
                 end = column_data.find('</adkim>')
-                adkim_substring = column_data[start:end]    
+                adkim_substring = column_data[start:end] 
+                
+            else: adkim_substring = 'NA'
     
     
             if '<aspf>' in column_data:
                 start = column_data.find('<aspf>') + len('<aspf>')
                 end = column_data.find('</aspf>')
-                aspf_substring = column_data[start:end]    
+                aspf_substring = column_data[start:end]  
+                
+            else: aspf_substring = 'NA'
     
             if '<p>' in column_data:
                 start = column_data.find('<p>') + len('<p>')
                 end = column_data.find('</p>')
-                p_substring = column_data[start:end]    
+                p_substring = column_data[start:end]  
+                
+            else: p_substring = 'NA'
     
     
             if '<sp>' in column_data:
                 start = column_data.find('<sp>') + len('<sp>')
                 end = column_data.find('</sp>')
-                sp_substring = column_data[start:end]    
+                sp_substring = column_data[start:end]   
+                
+            else: sp_substring = 'NA'
     
     
             if '<pct>' in column_data:
                 start = column_data.find('<pct>') + len('<pct>')
                 end = column_data.find('</pct>')
-                pct_substring = column_data[start:end]    
+                pct_substring = column_data[start:end] 
+                
+            else: pct_substring = 'NA'
 
+                
             if '<source_ip>' in column_data:
                 start = column_data.find('<source_ip>') + len('<source_ip>')
                 end = column_data.find('</source_ip>')
                 ip_substring = column_data[start:end]    
+                
+            else: ip_substring = 'NA'
+                
     
             if '<dkim>' in column_data:
                 start = column_data.find('<dkim>') + len('<dkim>')
                 end = column_data.find('</dkim>')
-                dkim_substring = column_data[start:end]    
+                dkim_substring = column_data[start:end]   
+           
+            else: dkim_substring = 'NA'
+                
   
             if '<disposition>' in column_data:
                 start = column_data.find('<disposition>') + len('<disposition>')
                 end = column_data.find('</disposition>')
-                disposition_substring = column_data[start:end]   
+                disposition_substring = column_data[start:end] 
+                
+            else: disposition_substring = 'NA'
 
             if '<spf>' in column_data:
                 start = column_data.find('<spf>') + len('<spf>')
                 end = column_data.find('</spf>')
                 spf_substring = column_data[start:end]    
+                
+            else: spf_substring = 'NA'
         
             if '<result>' in column_data:
                 start = column_data.find('<result>') + len('<result>')
                 end = column_data.find('</result>')
                 result_substring = column_data[start:end]    
+                
+            else: result_substring = 'NA'
     
         df = pd.DataFrame({'orgname':[org_substring],
                         'email': [email_substring],
@@ -210,7 +246,6 @@ def main():
             from parsons import Table
             file = s3.get_file(coc_bucket, x)
             file = Table.from_csv(file)
-            table_name=
                 
              #TODO: Table undeifned, may need to import from parsons?
             final_table = file_To_Table(file)
