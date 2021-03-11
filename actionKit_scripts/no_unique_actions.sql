a193ec0dc93cd3f8b5058f7c1a6d060b8b6362da
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2021-03-11 02:36:48 -0500

    Update and rename no_unique_actions to no_unique_actions.sql

--- actionKit_scripts/no_unique_actions.sql
+++ actionKit_scripts/no_unique_actions.sql
@@ -10,6 +10,5 @@ join coc_ak.core_action on (coc_ak.core_user.id = core_action.user_id)
 where 
   (select count(*) from coc_ak.core_action as ca where ca.user_id = core_user.id 
   and ca.source <> 'import' and ca.status = 'complete' and updated_at >= current_date - interval '9 month') < 1
-  and subscription_status = 'subscribed'
   and core_subscription.user_id not in (select user_id from coc_ak.core_open where core_open.created_at >= current_date - interval '9 month' )
   and core_subscription.user_id not in (select user_id from coc_ak.core_order where core_order.created_at >= current_date - interval '9 month')
